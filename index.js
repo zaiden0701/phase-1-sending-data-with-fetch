e7f9bc321a9ed5a0ca6f58dfbd0edfc9bc66089b
Aiden Zafar <+>
2022-10-11 21:44:08 -0400

    commit tree 93195efc4c9474723a8e8884a788bae08f53fd43

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Add your code here
+function submitData(name,email){
+    const configureObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+        })
+    }
+    return fetch('http://localhost:3000/users', configureObject)
+        .then(resp => resp.json())
+        .then(newUser => document.body.innerHTML = newUser['id'])
+        .catch(error => document.body.innerHTML = error.message)
+}
+
+// submitData()
+
+// function addUser(newUser){
+//     const p = document.createElement('p')
+//     p.innerHTML = newUser.id
+//     document.queryselector('body').append('p')
+// }
+
+// function submitData(name,email){
+//     return fetch('http://localhost:3000/users'),{
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           name,
+//           email,
+//         })
+//     }
+//         .then(resp => resp.JSON())
+//         .then(object => document.body.innerHTML = object['id'])
+//         .catch(error => document.body.innerHTML = error.message)
+// }
+// submitData()
